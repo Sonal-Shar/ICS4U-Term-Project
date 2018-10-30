53a7dc980b2915c520244c895b38d7b8d9fa6a48
Sonal Shar <32679670+Sonal-Shar@users.noreply.github.com>
2018-10-30 14:32:19 -0400

    Update Jeopardy.py

--- Jeopardy.py
+++ Jeopardy.py
@@ -2,33 +2,46 @@ import tkinter as tk
 from tkinter import *
 import time
 
-window = Tk()
-class popUp(Frame):
-    
-    def __init__(self):
-        super().__init__()   
-         
-        self.initUI()
+master = Tk()
+#class popUp(Frame):
+   
+#    def __init__(self):
+ #       super().__init__()   
+  #       
+   #     self.initUI()
         
         
-    def initUI(self):
-        window.geometry("600x400")
-        window.title("Settings for Jeopardy")
-        btn = Button(window, text="Games", command = Jeopardy.initUI)
-        btn.grid(column =1, row = 0)
+    #def initUI(self):
+   #     window.geometry("600x400")
+     #   window.title("Settings for Jeopardy")
+      #  btn = Button(window, text="Games", command = Jeopardy.initUI)
+       # btn.grid(column =1, row = 0)
 
         
 class Jeopardy(Frame):
-    
+        
     def initUI():
-        master = Tk()
+        def screen2():
+            print("Derp")
+            btn.grid_remove()
+        
         master.geometry("1400x800")
-        master.title("Jeopardy") 
+        master.title("Jeopardy")
+        user = int(input())
+        for buttons in range(0,user):
+            btn = Button(master, text = "Guess", command = screen2)
+            btn.grid(column = buttons, row = 6)
+            print(buttons)
+
+
+
 
 def main():
-    popUpGame = popUp()
-    window.mainloop()  
+   # popUpGame = popUp()
+    game = Jeopardy.initUI()
+    master.mainloop()  
 
 if __name__ == '__main__':
-    main()  
+    main()
+
 
