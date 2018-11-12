6c6594f1532af3c7a6141ca12b0efbb6bfb62c54
Sonal Sharma <sshar14@ocdsb.ca>
2018-11-12 00:25:04 -0500

    ARARHAHRHARHAHRHARHA

---
+++ AlphaTest/jeopardyGamePreset.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
+import time
+#master = Tk()
+points1 = 0
+points2 = 0
+points3 = 0
+points4 = 0
+
+class question(Frame):
+    def initUI():
+        window = Tk()
+        window.geometry("1400x800")
+        window.resizable(False, False)
+        window.title("Question")
+        
+        
+class Jeopardy(Frame):
+        
+    def initUI():
+        def screen2():
+            print("Derp")
+        
+        master.geometry("1400x800")
+        master.resizable(False, False)
+        master.title("Jeopardy")
+
+        
+        image = Image.open("jeopardywallpaper3.png")
+        photo = ImageTk.PhotoImage(image)
+        label = Label(image=photo)
+        label.image = photo # keep a reference!
+        label.place(x = 0, y = 0)
+
+        group1pts = Label(text = "$" + str(points1), font = ("Helvetica", 24), bg = 'white')
+        group1pts.place(x = 1060, y= 65)
+        image1 = Image.open("200btn.png")
+        image2 = Image.open("400btn.png")
+        image3 = Image.open("600btn.png")
+        image4 = Image.open("800btn.png")
+        image5 = Image.open("1000btn.png")
+        
+        BtnImg1 = ImageTk.PhotoImage(image1)
+        BtnImg2 = ImageTk.PhotoImage(image2)
+        BtnImg3 = ImageTk.PhotoImage(image3)
+        BtnImg4 = ImageTk.PhotoImage(image4)
+        BtnImg5 = ImageTk.PhotoImage(image5)
+        
+        c1r1 = Button(master, image = BtnImg1, command = screen2, height = 86, width = 185).place(x = 94, y = 285)
+        c1r2 = Button(master, image = BtnImg2, command = screen2, height = 86, width = 185).place(x = 94, y = 375)
+        c1r3 = Button(master, image = BtnImg3, command = screen2, height = 86, width = 185).place(x = 94, y = 465)
+        c1r4 = Button(master, image = BtnImg4, command = screen2, height = 86, width = 185).place(x = 94, y = 555)
+        c1r5 = Button(master, image = BtnImg5, command = screen2, height = 86, width = 185).place(x = 94, y = 645)
+
+        c2r1 = Button(master, image = BtnImg1, command = screen2, height = 86, width = 185).place(x = 299, y = 285)
+        c2r2 = Button(master, image = BtnImg2, command = screen2, height = 86, width = 185).place(x = 299, y = 375)
+        c2r3 = Button(master, image = BtnImg3, command = screen2, height = 86, width = 185).place(x = 299, y = 465)
+        c2r4 = Button(master, image = BtnImg4, command = screen2, height = 86, width = 185).place(x = 299, y = 555)
+        c2r5 = Button(master, image = BtnImg5, command = screen2, height = 86, width = 185).place(x = 299, y = 645)
+
+        c3r1 = Button(master, image = BtnImg1, command = screen2, height = 86, width = 185).place(x = 504, y = 285)
+        c3r2 = Button(master, image = BtnImg2, command = screen2, height = 86, width = 185).place(x = 504, y = 375)
+        c3r3 = Button(master, image = BtnImg3, command = screen2, height = 86, width = 185).place(x = 504, y = 465)
+        c3r4 = Button(master, image = BtnImg4, command = screen2, height = 86, width = 185).place(x = 504, y = 555)
+        c3r5 = Button(master, image = BtnImg5, command = screen2, height = 86, width = 185).place(x = 504, y = 645)
+
+        c4r1 = Button(master, image = BtnImg1, command = screen2, height = 86, width = 185).place(x = 709, y = 285)
+        c4r2 = Button(master, image = BtnImg2, command = screen2, height = 86, width = 185).place(x = 709, y = 375)
+        c4r3 = Button(master, image = BtnImg3, command = screen2, height = 86, width = 185).place(x = 709, y = 465)
+        c4r4 = Button(master, image = BtnImg4, command = screen2, height = 86, width = 185).place(x = 709, y = 555)
+        c4r5 = Button(master, image = BtnImg5, command = screen2, height = 86, width = 185).place(x = 709, y = 645)
+
+        c5r1 = Button(master, image = BtnImg1, command = screen2, height = 86, width = 185).place(x = 914, y = 285)
+        c5r2 = Button(master, image = BtnImg2, command = screen2, height = 86, width = 185).place(x = 914, y = 375)
+        c5r3 = Button(master, image = BtnImg3, command = screen2, height = 86, width = 185).place(x = 914, y = 465)
+        c5r4 = Button(master, image = BtnImg4, command = screen2, height = 86, width = 185).place(x = 914, y = 555)
+        c5r5 = Button(master, image = BtnImg5, command = screen2, height = 86, width = 185).place(x = 914, y = 645)
+
+
+        c6r1 = Button(master, image = BtnImg1, command = screen2, height = 86, width = 185).place(x = 1119, y = 285)
+        c6r2 = Button(master, image = BtnImg2, command = screen2, height = 86, width = 185).place(x = 1119, y = 375)
+        c6r3 = Button(master, image = BtnImg3, command = screen2, height = 86, width = 185).place(x = 1119, y = 465)
+        c6r4 = Button(master, image = BtnImg4, command = screen2, height = 86, width = 185).place(x = 1119, y = 555)
+        c6r5 = Button(master, image = BtnImg5, command = screen2, height = 86, width = 185).place(x = 1119, y = 645)
+        c1r1.image = BtnImg1
+
+    
+   
+def main():
+   # popUpGame = popUp()
+    #game = Jeopardy.initUI()
+    game = question.initUI()
+    #master.mainloop()  
+
+if __name__ == '__main__':
+    main()
---
+++ AlphaTest/mainMenuAndLogin.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter import *
+usernameList = open('usernames.txt').read().splitlines() #Collects data from the text file and stores it in an array
+passwordList = open('passwords.txt').read().splitlines() #Collects data from the text file and stores it in an array
+
+window = Tk()
+TextArea = Text(width = 26, height=1, font=("Helvetica", 26))
+TextArea2 = Text(width = 26, height=1, font=("Helvetica", 26))
+
+class mainMenuAndLogin():
+    def mainMenuGUI():
+        global btn
+        window.title("Main Menu")
+        btn= Button(window, text="Login",font=("Helvetica",15),command=mainMenuAndLogin.loginGUI, height = 6, width =18)
+        btn.place(x=570, y=350)
+
+        
+    def loginGUI():
+        global TextArea
+        global TextArea2
+        
+        window.title("Login")
+        btn.place_forget()
+        lab1= Label(window, text="Username",font=("Helvetica", 32))
+        lab1.place(x=350, y=300)
+        Label2=Label(window, text="Password", font=("Helvetica", 32))
+        Label2.place(x=350, y=350)
+
+        TextArea.place(x=570, y=300)
+
+        TextArea2.place(x=570, y=350)
+
+        btn2 = Button(window, text="Next",font=("Helvetica",15),command=mainMenuAndLogin.loginUI, height = 6, width = 18)
+        btn2.place(x=1030,y=510)
+        
+
+    def loginUI():
+        
+        def searchThroughFile(aList, anInteger): #Declare the function
+            tempVar = 0 #Variable that will hold index value
+            for i in range(len(aList)): #For the range in the length of the list
+                if int(aList[i]) == anInteger: #If the input = the index value
+                    tempVar = i + 1 #Tempvar is now the index place +1 since most people start counting with one
+                    return tempVar #returns the variable
+            return -1
+    
+        userNameSpot = 0
+        passwordNameSpot = 0
+        usernameInput = TextArea.get("1.0","end-1c")
+        passwordInput = TextArea2.get("1.0","end-1c")
+        tempUserVar = 37
+        tempPassVar = 37
+        
+        for i in range (len(usernameList)):
+            if usernameList[i] == usernameInput:
+                usernameSpot = 1
+                for j in range (len(passwordList)):
+                    if passwordList[i] == passwordInput:
+                        passwordSpot = 1
+                        if usernameSpot == passwordSpot:
+                            window.destroy()
+                            break
+                        else:
+                            print("Incorrect username or password. Please try again.")
+                            break
+                    else:
+                        tempPassVar -= 1
+                        if tempPassVar == 0:
+                            print("Incorrect username or password. Please try again.")
+                        
+            else:
+                tempUserVar -= 1
+                if tempUserVar == 0:
+                    print("Incorrect username or password. Please try again.")
+                
+                
+            
+        #print (userNameSpot)
+        #print(passwordNameSpot)
+        #if userNameSpot == passwordNameSpot:
+            #window.destroy()
+def main():
+    window.geometry('1400x800')
+    mainMenuAndLogin.mainMenuGUI()
+    #mainMenuAndLogin.loginUI()
+
+
+if __name__ == "__main__":
+    main()
+    window.mainloop()
