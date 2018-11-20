852d9c9b9d797187b0f2a7e2476a0fb66b7cc70b
SONAL SHARMA <S328157433@OCDSB>
2018-11-20 14:21:51 -0500

    adaodjaoihda

---
+++ Post-Alpha Test/directory.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+
+root = Tk()
+
+class MainView(Frame):
+    def page1():
+        btn1 = 
+if __name__ == "__main__":
+    main = MainView()
+    root.geometry('1400x800')
+    root.mainloop()
